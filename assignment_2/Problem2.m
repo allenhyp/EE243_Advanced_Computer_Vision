2c022871030aae2c8bbf956c7e2934b67e955741
Allen Hsu <bboypupu@gmail.com>
2018-04-29 23:57:04 -0700

    report and compress

--- assignment_2/Problem2.m
+++ assignment_2/Problem2.m
@@ -19,15 +19,24 @@ subplot(2,2,3), imagesc(v2), title('verticle detail');
 subplot(2,2,4), imagesc(d2), title('diagonal detail');
 
 % drop high frequency (detail) components
-% d_d2 = drop_high_f_compo(d2);
-re_img_1 = idwt2(a2,h2,v2,d2,'haar',M);
+d_d2 = drop_high_f_compo(d2);
+d_v2 = drop_high_f_compo(v2);
+d_h2 = drop_high_f_compo(h2);
+d_a2 = drop_high_f_compo(a2);
+
+re_img_1 = idwt2(d_a2,d_h2,d_v2,d_d2,'haar',M);
 max(max(abs(re_img_1-a)))
 
 figure;
 subplot(1,2,1), imagesc(a), colormap(gray), title('approximation level 1');
 subplot(1,2,2), imagesc(re_img_1), colormap(gray), title('reconstruct level 2');
 
-re_img_0 = idwt2(a,h,v,d,'haar',M);
+d_d = drop_high_f_compo(d);
+d_v = drop_high_f_compo(v);
+d_h = drop_high_f_compo(h);
+d_a = drop_high_f_compo(a);
+
+re_img_0 = idwt2(d_a,d_h,d_v,d_d,'haar',M);
 max(max(abs(re_img_0-img)))
 
 figure;
@@ -42,12 +51,12 @@ function amplitude_image = drop_high_f_compo(input_image)
     f_image = fftshift(fft2(input_image));
     a_image = log(abs(f_image));
     subplot(2,3,1), imshow(a_image, []);
-    amplitude_threshold = 8.0;
+    amplitude_threshold = 9.8;
     bright_spikes = a_image > amplitude_threshold; % Binary image.
-    subplot(2,3,2), imshow(bright_spikes);
+    subplot(2,3,2), imagesc(bright_spikes);
     % Exclude the central DC spike.  Everything from row 115 to 143.
-    bright_spikes(115:143, :) = 0;
-    subplot(2,3,3), imshow(bright_spikes);
+    bright_spikes(50:200, :) = 0;
+    subplot(2,3,3), imagesc(bright_spikes);
     % Filter/mask the spectrum.
     f_image(bright_spikes) = 0;
     % Take log magnitude so we can see it better in the display.
--- assignment_2/Problem3a.m
+++ assignment_2/Problem3a.m
@@ -11,18 +11,19 @@ for delta=0.0:0.001:0.004
     bw_house_log = edge(img_house,'log',threshold+delta);
     bw_house_canny = edge(img_house,'Canny',[(threshold+delta)*10 (threshold+delta)*50]);
 
-    subplot(col,2,count), imshow(bw_house_log);
-    subplot(col,2,count+1), imshow(bw_house_canny);
+    subplot(col,2,count), imshow(bw_house_log), title(sprintf('LoG: threshold %f',(threshold+delta)));
+    subplot(col,2,count+1), imshow(bw_house_canny), title(sprintf('Canny: threshold[%f, %f]',(threshold+delta)*10,(threshold+delta)*50));
     count = count+2;
 end
 
 count = 1;
+threshold = 0.0025;
 figure;
 for delta=0.0:0.001:0.004
     bw_lena_log = edge(img_lena,'log',threshold+delta);
     bw_lena_canny = edge(img_lena,'Canny',[(threshold+delta)*10 (threshold+delta)*100]);
 
-    subplot(col,2,count), imshow(bw_lena_log);
-    subplot(col,2,count+1), imshow(bw_lena_canny);
+    subplot(col,2,count), imshow(bw_lena_log), title(sprintf('LoG: threshold %f',(threshold+delta)));
+    subplot(col,2,count+1), imshow(bw_lena_canny), title(sprintf('Canny: threshold[%f, %f]',(threshold+delta)*10,(threshold+delta)*50));
     count = count+2;
 end
