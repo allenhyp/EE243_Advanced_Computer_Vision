b4c6d2402cf74b881845d58582cc61e1ffc0b98b
Allen Hsu <bboypupu@gmail.com>
2018-05-12 17:19:42 -0700

    Detect bounding boxes

--- assignment_3/codes/getDetections.m
+++ assignment_3/codes/getDetections.m
@@ -5,17 +5,32 @@ function bbox = getDetections(D)
 
 % YOUR CODE HERE. DO NOT CHANGE ANYTHING ABOVE THIS.
     d = D./max(D(:,:));
-    d = medfilt2(d, [5, 5]) > 0.5;
-    figure; imshow(d*255);
-    se = strel('disk', 10);
-    c = imclose(d, se);
+    d = imgaussfilt(medfilt2(d, [5, 5]), 0.5) > 0.5;
+%     figure; imshow(d*255);
+
+    se_d = strel('disk', 20);
+    c = imclose(d, se_d);
+    se_l = strel('line', 2, 10);
+    c = imopen(c, se_l);
     CC = bwconncomp(c);
-    stats = regionprops(CC, 'BoundingBox');
-    figure; imshow(c*255); hold on
-    for i = 1:length(stats)
-        cur_box = stats(i).BoundingBox;
-        rectangle('Position', [cur_box(1), cur_box(2), cur_box(3), cur_box(4)], 'EdgeColor','r','LineWidth',2);
+    stats = regionprops(CC, 'BoundingBox', 'Area');
+    props = struct2cell(stats);
+    props = props';
+    props = flipud(sortrows(props, 1));
+    N = size(props,1);    
+    cnt = 0;
+%     figure; imshow(c*255); hold on
+    for i = 1:N
+        cur_box = props{i,2};
+%         rectangle('Position', [cur_box(1), cur_box(2), cur_box(3), cur_box(4)], 'EdgeColor','r','LineWidth',2);
         bbox(i,:) = cur_box;
+        cnt=cnt+1;
+        if cnt >= 5
+            break
+        end
     end
-    hold off
+%     hold off
+    if N == 0 
+        bbox = zeros(0,4);
+    end
 end
--- assignment_3/codes/getSumOfDiff.m
+++ assignment_3/codes/getSumOfDiff.m
@@ -3,10 +3,10 @@ function sod = getSumOfDiff(I)
 % I is a 3D tensor of image sequence where the 3rd dimention represents the time axis
 
 % YOUR CODE HERE. DO NOT CHANGE ANYTHING ABOVE THIS.
-    figure;
-    subplot(3,1,1); imshow(I(:,:,1));
-    subplot(3,1,2); imshow(I(:,:,2));
-    subplot(3,1,3); imshow(I(:,:,3));
+%     figure;
+%     subplot(3,1,1); imshow(I(:,:,1));
+%     subplot(3,1,2); imshow(I(:,:,2));
+%     subplot(3,1,3); imshow(I(:,:,3));
     [m, n, N] = size(I);
     sod = zeros(m, n);
     for i = 1:N-1
