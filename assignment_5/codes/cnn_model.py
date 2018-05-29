367e24fe82fc16e69f78954815111331a8f1b2c9
Allen Hsu <bboypupu@gmail.com>
2018-05-29 15:34:59 -0700

    test 2 convolutional layers and logits layer in the end

--- assignment_5/codes/cnn_model.py
+++ assignment_5/codes/cnn_model.py
@@ -1,14 +1,55 @@
 import tensorflow as tf
+import os, re, sys
 
 def _variable_with_weight_decay(name, shape, wd):
-  with tf.device('/cpu:0'):
-    var = tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer())
-  if wd is not None:
-    weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
-    tf.add_to_collection('losses', weight_decay)
-  return var  
+    with tf.device('/cpu:0'):
+        var = tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer())
+    if wd is not None:
+        weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
+        tf.add_to_collection('losses', weight_decay)
+    return var  
+
+
+def _variable_on_cpu(name, shape, initializer):
+    with tf.device('/cpu:0'):
+        dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+        var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
+    return var
+
+
+def _activation_summary(x):
+    tensor_name = re.sub('%s_[0-9]*/' % TOWER_NAME, '', x.op.name)
+    tf.summary.histogram(tensor_name + '/activations', x)
+    tf.summary.scalar(tensor_name + '/sparsity',tf.nn.zero_fraction(x))
+
 
 def inference(X,phase=False,dropout_rate=0.8,n_classes=10,weight_decay=1e-4):
     # logits should be of dimension (batch_size, n_classes)
-    
+    # 1st conv layer with maxpool, normalization, and dropout
+    with tf.variable_scope('conv1') as scope:
+        kernel = _variable_with_weight_decay('weights', shape=[5, 5, 3, 64], stddev=5e-2, wd=weight_decay)
+        conv = tf.nn.conv2d(X, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv1 = tf.nn.relu(pre_activation, name=scope.name)
+        _activation_summary(conv1)
+    pool1 = tf.nn.maxpool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool1')
+    norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001/9.0, beta=0.75, name='norm1')
+    dropout1 = tf.layers.dropout(inputs=norm1, rate=dropout_rate, training=True)
+
+    # 2nd conv layer with maxpool, normalization, and dropout
+    with tf.variable_scope('conv2') as scope:
+        kernel = _variable_with_weight_decay('weights', shape=[5, 5, 64, 64], stddev=5e-2, wd=weight_decay)
+        conv = tf.nn.conv2d(X, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv1 = tf.nn.relu(pre_activation, name=scope.name)
+        _activation_summary(conv2)
+    pool2 = tf.nn.maxpool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+    norm2 = tf.nn.lrn(pool2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name='norm2')
+    dropout2 = tf.layers.dropout(inputs=norm2, rate=dropout_rate, training=True)
+
+    # logits layer
+    logits = tf.layers.dense(inputs=dropout2, units=10)
+
     return logits
