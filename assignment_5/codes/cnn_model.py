ceb91e27f75d317069f271a3967977880b67e1f3
Allen Hsu <bboypupu@gmail.com>
2018-05-31 12:56:26 -0700

    checkpoint2

--- assignment_5/codes/cnn_model.py
+++ assignment_5/codes/cnn_model.py
@@ -45,28 +45,28 @@ def _variable_with_weight_decay(name, shape, wd):
 #     tf.summary.scalar(tensor_name + '/sparsity',tf.nn.zero_fraction(x))
 
 
-def batch_norm(input_, name, n_out, phase_train):
-    with tf.variable_scope(name + 'bn'):
-        beta = tf.get_variable(tf.constant(0.0, shape=[n_out]), name=name + 'beta', trainable=True)
-        gamma = tf.get_variable(tf.constant(1.0, shape=[n_out]), name=name + 'gamma', trainable=True)
-        if len(input_.get_shape().as_list()) > 3:
-            batch_mean, batch_var = tf.nn.moments(input_, [0, 1, 2], name=name + 'moments')
-        else:
-            batch_mean, batch_var = tf.nn.moments(input_, [0, 1], name=name + 'moments')
-        ema = tf.train.ExponentialMovingAverage(decay=0.5)
+# def batch_norm(input_, name, n_out, phase_train):
+#     with tf.variable_scope(name + 'bn'):
+#         beta = tf.get_variable(tf.constant(0.0, shape=[n_out]), name=name + 'beta', trainable=True)
+#         gamma = tf.get_variable(tf.constant(1.0, shape=[n_out]), name=name + 'gamma', trainable=True)
+#         if len(input_.get_shape().as_list()) > 3:
+#             batch_mean, batch_var = tf.nn.moments(input_, [0, 1, 2], name=name + 'moments')
+#         else:
+#             batch_mean, batch_var = tf.nn.moments(input_, [0, 1], name=name + 'moments')
+#         ema = tf.train.ExponentialMovingAverage(decay=0.5)
 
-        def mean_var_with_update():
-            ema_apply_op = ema.apply([batch_mean, batch_var])
-            with tf.control_dependencies([ema_apply_op]):
-                return tf.identity(batch_mean), tf.identity(batch_var)
+#         def mean_var_with_update():
+#             ema_apply_op = ema.apply([batch_mean, batch_var])
+#             with tf.control_dependencies([ema_apply_op]):
+#                 return tf.identity(batch_mean), tf.identity(batch_var)
 
-        mean, var = tf.cond(phase_train, mean_var_with_update, lambda: (
-            ema.average(batch_mean), ema.average(batch_var)))
-        normed = tf.nn.batch_normalization(input_, mean, var, beta, gamma, 1e-3)
+#         mean, var = tf.cond(phase_train, mean_var_with_update, lambda: (
+#             ema.average(batch_mean), ema.average(batch_var)))
+#         normed = tf.nn.batch_normalization(input_, mean, var, beta, gamma, 1e-3)
 
-    variable_summaries(beta)
-    variable_summaries(gamma)
-    return normed
+#     variable_summaries(beta)
+#     variable_summaries(gamma)
+#     return normed
 
 
 def parametric_relu(input_, name):
@@ -85,12 +85,11 @@ def conv(input_, name, k, n_o, wd, is_tr, s=1, is_act=True, is_bn=True, padding=
     n_i = input_.get_shape()[-1].value
     with tf.variable_scope(name):
         kernel = _variable_with_weight_decay('weights', shape=[k, k, n_i, n_o], wd=wd)
-        weights = tf.get_variable(name + "weights",
-                                  kernel,
-                                  tf.float32, xavier_initializer())
+        print(kernel)
+        weights = tf.get_variable(name + "weights", kernel, tf.float32, xavier_initializer())
         biases = tf.get_variable(name +"bias", [n_o], tf.float32, tf.constant_initializer(0.0))
         conv = tf.nn.conv2d(input_, weights, (1, s, s, 1), padding=padding)
-        bn = batch_norm(conv, name, n_o, is_tr) if is_bn else conv
+        bn = tf.layers.batch_normalization(conv, axis=-1, training=is_tr, name='bn')
         activation = parametric_relu(tf.nn.bias_add(bn, biases), name + "activation") if is_act else tf.nn.bias_add(bn, biases)
         variable_summaries(weights)
         variable_summaries(biases)
